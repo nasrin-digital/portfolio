0ac70ce5f002ce63719b5dba6c8daeea6bcbc42e
Nasrin Mostajeran <nasrin.mostajeran@gmail.com>
2021-11-27 22:49:35 -0800

    add contact form and footer

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -3,6 +3,34 @@ var carousel = new bootstrap.Carousel(myCarousel, {
   interval: 2000,
   wrap: false
 })
-myCarousel.addEventListener('slid.bs.carousel', function (event) {
-    carousel.to('2') // Will slide to the slide 2 as soon as the transition to slide 1 is finished
-})
+
+// Form validation
+$(document).ready(function() {
+  $('#contact-form').submit(function () {
+    // Get the Name value
+    var name = $('#name').val();
+    // Check if empty of not
+    if (name  === '') {
+        alert('Enter the name.');
+        return false;
+    }
+    // Get the email value
+    var email = $('#email').val();
+    // Check if empty of not
+    if (email  === '') {
+        alert('Enter the email address.');
+        return false;
+    }
+    // Get the subject value
+    var subject = $('#subject').val();
+    // Check if empty of not
+    if (subject  === '') {
+        alert('Enter the subject.');
+        return false;
+    }
+
+    $("#contact-message").css('color', 'green');
+    $("#contact-message").html("Thanks for contacting me. I'll get back to get you soon!");
+    return false; // just to stay here. it won't go to backend
+  });
+});

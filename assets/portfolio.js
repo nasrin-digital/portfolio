ecec4bdc56daed1d2707e595ae08e7bfe8bba422
Nasrin Mostajeran <nasrin.mostajeran@gmail.com>
2021-11-29 20:07:54 -0800

    add user input validation and cleanup codes

--- assets/portfolio.js
+++ assets/portfolio.js
@@ -6,6 +6,9 @@ var carousel = new bootstrap.Carousel(myCarousel, {
 
 // Form validation
 $(document).ready(function() {
+  var form = document.getElementById("contact-form");
+  function handleForm(event) { event.preventDefault(); }  // stop the page from reloading after submit
+  form.addEventListener('submit', handleForm);
   $('#contact-form').submit(function () {
     // Get the Name value
     var name = $('#name').val();
@@ -21,6 +24,11 @@ $(document).ready(function() {
         alert('Enter the email address.');
         return false;
     }
+    if(!isEmail(email)){
+      $("#contact-message").css('color', 'black');
+      $("#contact-message").html("You have entered an invalid email address!");
+      return false;
+    }
     // Get the subject value
     var subject = $('#subject').val();
     // Check if empty of not
@@ -34,3 +42,8 @@ $(document).ready(function() {
     return false; // just to stay here. it won't go to backend
   });
 });
+
+function isEmail(email) {
+  var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
+  return regex.test(email);
+}
